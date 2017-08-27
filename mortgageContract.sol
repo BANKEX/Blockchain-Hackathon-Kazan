266eb23e6abaa53f4884f9d56236b6192ff45143
Lana Ivina <svetlana.konstantinovna@gmail.com>
2017-08-27 22:53:32 +0300

    Добавила верификацию подписей кредитора и заемщика, написала функцию ежемесячного платежа и др.

--- mortgageContract.sol
+++ mortgageContract.sol
@@ -7,14 +7,33 @@ contract MortgageContract {
 
     uint price;
 
-    int srok_ipoteki;
+    int time;
 
     int monthly_payment;
+
     enum state {under_construction, rented_out, paid}
 
     address public borrower;
 
     address public lender;
+
+    struct Verify {
+        bool borrower_signature;
+        bool lender_signature;
+    }
+
+    ///API Sign.Me
+
+    function sign_Deal {
+        require();
+        uint moment_Deal = now;
+        require((Verify.borrower_signature && Verify.lender_signature) = True);
+
+    }
+
+    function getMonthlyPayment () payable returns (bool) {
+        lender.send(this.monthly_payment);
+    }
 
     struct Mortgage {
     uint balance_borrower;
@@ -24,26 +43,31 @@ contract MortgageContract {
 
     Mortgage mortgage;
 
+    mortgage.balance_borrower = price;
+
+
     function checkClosed returns (bool) {
 
         bool returnStatus = false;
 
-        for (uint i = 0; i < srok_ipoteki; i++) {
-            zakladnaya.balance_borrower = price - i * monthly_payment;
-            if (zakladnaya.status_borrower == true) {
+        for (uint i = 0; i < time; i++) {
+            mortgage.balance_borrower = price - i * monthly_payment;
+            if (balance_borrower > 0) {
+                    mortgage.status_borrower = false;
+            } else {
+                mortgage.status_borrower = true;
+            }
+            if (mortgage.status_borrower == true) {
                 returnStatus = true;
                 break;
             }
-
         }
         return returnStatus;
     }
 
-
     address public construction;
 
     event ipoteka_paid();
-
 
     function ipoteka() {
         // constructor
