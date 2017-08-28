4f0527fea4b6d3a84aa405dd5c13d47be23abf5b
dnx2k <denis.khoruzhiy@bankex.co>
2017-08-28 13:55:22 +0300

    contract deploying

--- webapp/controllers/pages.js
+++ webapp/controllers/pages.js
@@ -1,3 +1,5 @@
+var ethAdapter = require('../eth/adapter');
+
 exports.home = function (req, res) {
 	res.render('pages/home', {
 		title: 'Home'
@@ -7,9 +9,9 @@ exports.home = function (req, res) {
 exports.deploy = function (req, res) {
 	res.render('pages/deploy', {
 		title: 'Создание контракта',
-		bank: '0x24ecbda81f238aca238e68d339c583c0cece865a',
-		borrower: '0x24ecbda81f238aca238e68d339c583c0c234g343',
-		depository:	'0x24ecbda81f238aca238e68d339c583c0c342265a',
-		registry: '0x24ecbda81f238aca238e68d339c583c0cece865a',
+		bank: ethAdapter.accounts[0],
+		borrower: ethAdapter.accounts[1],
+		depository:	ethAdapter.accounts[2],
+		registry: ethAdapter.accounts[3],
 	})
 }
--- webapp/public/scripts.js
+++ webapp/public/scripts.js
@@ -1,13 +1,31 @@
 $("document").ready(function () {
+    $("form#deploy").submit(function (event) {
+        event.preventDefault();
+
+        var unindexed_array = $(this).serializeArray();
+        var indexed_array = {};
+
+        $.map(unindexed_array, function(n, i){
+            indexed_array[n['name']] = n['value'];
+        });
+
+        $("#spinner").show();
+
+        $.ajax({ url: 'api/v1/contracts/deploy', type: 'POST', contentType: 'application/json', data: JSON.stringify(indexed_array), success: function (data) {
+            $("#spinner").hide();
+
+            alert("Контракт успешно опубликован по адресу\n" + data.address);
+        }});
+    });
 });
 
 function addCoborrower() {
-	var el = $('<div class="form-group"><label for="coborrower" class="col-sm-2 control-label">Созаёмщик</label><div class="col-sm-10"><div class="input-group"><input type="text" class="form-control" id="coborrower" placeholder="Адрес созаёмщика" value=""><span class="input-group-btn"><button type="button" class="btn btn-default" onclick="setMetamaskAddress(this)" title="Получить из Metamask">@</button></span></div></div>');
-	var target = $('#extra');
-	target.replaceWith(el);
+    var el = $('<div class="form-group"><label for="coborrower" class="col-sm-2 control-label">Созаёмщик</label><div class="col-sm-10"><div class="input-group"><input type="text" class="form-control" id="coborrower" placeholder="Адрес созаёмщика" name="coborrower" value=""><span class="input-group-btn"><button type="button" class="btn btn-default" onclick="setMetamaskAddress(this)" title="Получить из Metamask">@</button></span></div></div>');
+    var target = $('#extra');
+    target.replaceWith(el);
 }
 
 function setMetamaskAddress(el) {
-	var input = $(el).parent().prev();
-	var address = web3.eth.getAccounts(function (err, accounts) { input.val(accounts[0]); });
+    var input = $(el).parent().prev();
+    var address = web3.eth.getAccounts(function (err, accounts) { input.val(accounts[0]); });
 }
